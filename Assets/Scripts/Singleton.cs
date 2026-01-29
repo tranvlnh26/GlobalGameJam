39f6c79971830f5b6ad5d39fc8c57d6d57351a2c
tranvlnh <92441399+tranvlnh26@users.noreply.github.com>
2026-01-29 23:12:56 +0700

    Add generic Singleton MonoBehaviour implementation
    Introduces a Singleton class for Unity that ensures only one instance exists and persists across scenes. This utility can be inherited by other MonoBehaviours to easily implement the singleton pattern.

---
+++ Assets/Scripts/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+public class Singleton : MonoBehaviour
+{
+    private static Singleton _instance;
+
+    public static Singleton Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = FindAnyObjectByType<Singleton>();
+                if (_instance == null)
+                {
+                    var singletonObject = new GameObject(nameof(Singleton));
+                    _instance = singletonObject.AddComponent<Singleton>();
+                    DontDestroyOnLoad(singletonObject);
+                }
+            }
+            return _instance;
+        }
+    }
+
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as Singleton;
+            DontDestroyOnLoad(gameObject);
+        }
+        else if (_instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
